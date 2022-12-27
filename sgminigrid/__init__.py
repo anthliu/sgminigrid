7debf7ac442a881fbaa72cc4e9a16d99ec8e452e
Anthony Liu <anthliu@umich.edu>
2022-12-27 14:59:40 -0500

    Revert "rename env"
    This reverts commit a59b392eb204ba8232745c0e29c15c45b4a4599a.

--- sgminigrid/__init__.py
+++ sgminigrid/__init__.py
@@ -7,16 +7,16 @@ from minigrid.core import roomgrid
 
 def register_sgminigrid_envs():
     register(
-        id="SGMG/ButtonTest-v0",
+        id="SGMG-ButtonTest-v0",
         entry_point="sgminigrid.envs.buttontest:ButtonEnv"
     )
     register(
-        id="SGMG/ButtonDoor-v0",
+        id="SGMG-ButtonDoor-v0",
         entry_point="sgminigrid.envs.buttondoor:ButtonDoorEnv",
         kwargs={"size": 8, "num_extra_buttons": 1, "max_steps": 100}
     )
     register(
-        id="SGMG/Empty-v0",
+        id="SGMG-Empty-v0",
         entry_point="sgminigrid.envs.empty:SGEmptyEnv",
         kwargs={"size": 8, "max_steps": 50}
     )
