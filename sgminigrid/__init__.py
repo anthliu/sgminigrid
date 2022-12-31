6ec5cd231254f98b77888dd38e82fdfae9c154fc
Anthony Liu <anthliu@umich.edu>
2022-12-31 14:22:29 -0500

    add more complex door button env

--- sgminigrid/__init__.py
+++ sgminigrid/__init__.py
@@ -16,6 +16,11 @@ def register_sgminigrid_envs():
         kwargs={"size": 7, "num_colors": 4, "num_extra_buttons": 3, "max_steps": 100}
     )
     register(
+        id="SGMG-ButtonDoorL-v0",
+        entry_point="sgminigrid.envs.buttondoor:ButtonDoorEnv",
+        kwargs={"size": 7, "num_colors": 6, "num_extra_buttons": 4, "max_steps": 100}
+    )
+    register(
         id="SGMG-Empty-v0",
         entry_point="sgminigrid.envs.empty:SGEmptyEnv",
         kwargs={"size": 8, "max_steps": 50}
